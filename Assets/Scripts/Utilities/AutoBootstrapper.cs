968b0424c0d69713175befad26385ca4723049a6
Raphael <salandananry@gmail.com>
2025-12-02 23:04:55 +0800

    Add bootstrap

---
+++ Assets/Scripts/Utilities/AutoBootstrapper.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoBootstrapper
+{
+    private const string BootstrapSceneName = "Bootstrap";
+    private static bool hasRedirected = false;
+    public static string sceneToLoad;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void RedirectToBootstrap()
+    {
+        var activeScene = SceneManager.GetActiveScene().name;
+        sceneToLoad = activeScene;
+        if (activeScene == BootstrapSceneName)
+            return;
+
+        if (hasRedirected)
+            return;
+
+        hasRedirected = true;
+
+        SceneManager.LoadScene(BootstrapSceneName, LoadSceneMode.Single);
+    }
+}
